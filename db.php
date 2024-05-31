042c6023b3d0760d0c480f018e8c57ee6fc61302
Vignesh M <mvigneshcse21@gmail.com>
2024-05-31 16:44:00 +0530

    deleted some files

--- db.php
+++
@@ -1,6 +0,0 @@
-<?php
-$host = "localhost"; // Your database host
-$username = "admin"; // Your database username
-$password = ""; // Your database password
-$database = "movieinfo"; // Your database name
-?>
