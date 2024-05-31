952925f231c7be4294a61323de9d19ee51757ca2
Vignesh M <mvigneshcse21@gmail.com>
2024-05-31 15:51:21 +0530

    Push

---
+++ db.php
@@ -0,0 +1,6 @@
+<?php
+$host = "localhost"; // Your database host
+$username = "admin"; // Your database username
+$password = ""; // Your database password
+$database = "movieinfo"; // Your database name
+?>
